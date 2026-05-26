27b86b297ab67bc236dbde3d69cd395e079ff455
JAY <JAY@oonne.com>
2026-05-26 16:37:17 +0800

    feat: 脚本更新

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,11 @@
+#是否递增版本号和构件号
+INCREMENT_VERSION=false
+#是否打包ios
+PACK_IOS=false
+#是否打包android
+PACK_ANDROID=true
+
+
 # 记录开始时间
 START_TIME=$(date +%s)
 
@@ -14,62 +22,77 @@ echo "开始打包: $PACKAGE_NAME"
 VERSION_LINE=$(grep "version:" pubspec.yaml)
 VERSION=$(echo $VERSION_LINE | cut -d'+' -f1 | cut -d' ' -f2)
 BUILD_NUMBER=$(echo $VERSION_LINE | cut -d'+' -f2)
-# 版本号递增逻辑
-IFS='.' read -r -a version_parts <<< "$VERSION"
-major=${version_parts[0]}
-minor=${version_parts[1]}
-patch=${version_parts[2]}
-# 补丁版本号+1
-new_patch=$((patch + 1))
-new_minor=$minor
-new_major=$major
-# 如果补丁版本号超过9，进位到次版本号
-if [ $new_patch -gt 9 ]; then
-    new_patch=0
-    new_minor=$((minor + 1))
+
+if [ "$INCREMENT_VERSION" = true ]; then
+    # 版本号递增逻辑
+    IFS='.' read -r -a version_parts <<< "$VERSION"
+    major=${version_parts[0]}
+    minor=${version_parts[1]}
+    patch=${version_parts[2]}
+    # 补丁版本号+1
+    new_patch=$((patch + 1))
+    new_minor=$minor
+    new_major=$major
+    # 如果补丁版本号超过9，进位到次版本号
+    if [ $new_patch -gt 9 ]; then
+        new_patch=0
+        new_minor=$((minor + 1))
+    fi
+    # 如果次版本号超过9，进位到主版本号
+    if [ $new_minor -gt 9 ]; then
+        new_minor=0
+        new_major=$((major + 1))
+    fi
+    # 构建新的版本号，写入文件
+    NEW_VERSION="$new_major.$new_minor.$new_patch"
+    NEW_BUILD_NUMBER=$((BUILD_NUMBER + 1))
+    sed -i '' "s/version: $VERSION+$BUILD_NUMBER/version: $NEW_VERSION+$NEW_BUILD_NUMBER/" pubspec.yaml
+    echo "版本号: $NEW_VERSION ($NEW_BUILD_NUMBER)"
+else
+    NEW_VERSION=$VERSION
+    NEW_BUILD_NUMBER=$BUILD_NUMBER
+    echo "跳过版本号递增，当前版本号: $NEW_VERSION ($NEW_BUILD_NUMBER)"
 fi
-# 如果次版本号超过9，进位到主版本号
-if [ $new_minor -gt 9 ]; then
-    new_minor=0
-    new_major=$((major + 1))
-fi
-# 构建新的版本号，写入文件
-NEW_VERSION="$new_major.$new_minor.$new_patch"
-NEW_BUILD_NUMBER=$((BUILD_NUMBER + 1))
-sed -i '' "s/version: $VERSION+$BUILD_NUMBER/version: $NEW_VERSION+$NEW_BUILD_NUMBER/" pubspec.yaml
-echo "版本号: $NEW_VERSION ($NEW_BUILD_NUMBER)"
 
 # 打包ipa
-flutter build ipa --obfuscate --split-debug-info=scripts/symbols
-# 判断 打包后的 ipa 文件是否存在
-if [ -f "build/ios/ipa/$PACKAGE_NAME.ipa" ]; then
-    echo "ipa 打包成功"
-else
-    echo "ipa 打包失败"
-    exit 1
-fi
-# 复制 ipa 文件到scripts/app/ 并重命名为 package_name.ipa
-cp "build/ios/ipa/$PACKAGE_NAME.ipa" "$SCRIPT_DIR/app/$PACKAGE_NAME-$NEW_VERSION.ipa"
-# 删除 打包后的 ipa 文件
-rm -f "build/ios/ipa/$PACKAGE_NAME.ipa"
-
-# 打包 apk - 多个 flavor
-FLAVORS=("googleplay" "xiaomi" "oppo" "vivo" "honor")
-for FLAVOR in "${FLAVORS[@]}"; do
-    echo "开始打包 $FLAVOR flavor"
-    flutter build apk --flavor $FLAVOR --obfuscate --split-debug-info=scripts/symbols-$FLAVOR
-    # 判断 打包后的 apk 文件是否存在
-    if [ -f "build/app/outputs/flutter-apk/$FLAVOR/release/app-$FLAVOR-release.apk" ]; then
-        echo "apk $FLAVOR 打包成功"
-        # 复制 apk 文件到 scripts/app/ 并重命名为 package_name-flavor.apk
-        cp "build/app/outputs/flutter-apk/$FLAVOR/release/app-$FLAVOR-release.apk" "$SCRIPT_DIR/app/$PACKAGE_NAME-$NEW_VERSION-$FLAVOR.apk"
-        # 删除 打包后的 apk 文件
-        rm -f "build/app/outputs/flutter-apk/$FLAVOR/release/app-$FLAVOR-release.apk"
+if [ "$PACK_IOS" = true ]; then
+    flutter build ipa --obfuscate --split-debug-info=scripts/symbols
+    # 判断 打包后的 ipa 文件是否存在
+    if [ -f "build/ios/ipa/$PACKAGE_NAME.ipa" ]; then
+        echo "ipa 打包成功"
+        # 复制 ipa 文件到scripts/app/ 并重命名为 package_name.ipa
+        cp "build/ios/ipa/$PACKAGE_NAME.ipa" "$SCRIPT_DIR/app/$PACKAGE_NAME-$NEW_VERSION.ipa"
+        # 删除 打包后的 ipa 文件
+        rm -f "build/ios/ipa/$PACKAGE_NAME.ipa"
     else
-        echo "apk $FLAVOR 打包失败"
+        echo "ipa 打包失败"
         exit 1
     fi
-done
+else
+    echo "跳过 iOS 打包"
+fi
+
+# 打包 apk - 多个 flavor
+if [ "$PACK_ANDROID" = true ]; then
+    FLAVORS=("googleplay" "xiaomi" "oppo" "vivo" "honor")
+    for FLAVOR in "${FLAVORS[@]}"; do
+        echo "开始打包 $FLAVOR flavor"
+        flutter build apk --flavor $FLAVOR --obfuscate --split-debug-info=scripts/symbols-$FLAVOR
+        # 判断 打包后的 apk 文件是否存在
+        if [ -f "build/app/outputs/flutter-apk/$FLAVOR/release/app-$FLAVOR-release.apk" ]; then
+            echo "apk $FLAVOR 打包成功"
+            # 复制 apk 文件到 scripts/app/ 并重命名为 package_name-flavor.apk
+            cp "build/app/outputs/flutter-apk/$FLAVOR/release/app-$FLAVOR-release.apk" "$SCRIPT_DIR/app/$PACKAGE_NAME-$NEW_VERSION-$FLAVOR.apk"
+            # 删除 打包后的 apk 文件
+            rm -f "build/app/outputs/flutter-apk/$FLAVOR/release/app-$FLAVOR-release.apk"
+        else
+            echo "apk $FLAVOR 打包失败"
+            exit 1
+        fi
+    done
+else
+    echo "跳过 Android 打包"
+fi
 
 # 全部打包完成
 END_TIME=$(date +%s)
