a0ce5c40595e7ef81dbbd09de11e59530e4683a0
JAY <JAY@oonne.com>
2026-05-27 23:00:08 +0800

    fix: 删除冗余引用

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
 
-import 'package:flutter_demo/utils/log.dart';
 import 'home_view_model.dart';
 
 /* 
