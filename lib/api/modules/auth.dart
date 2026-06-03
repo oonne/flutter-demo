e007e53e0d679d152dc42e58275e9d432f269031
JAY <JAY@oonne.com>
2026-06-03 16:37:57 +0800

    feat: 类型定义

--- lib/api/modules/auth.dart
+++ lib/api/modules/auth.dart
@@ -1,24 +1,21 @@
 import 'package:flutter_demo/api/http.dart';
 
 class AuthApi {
-  // 获取登录的pow校验
-  static getLoginPow(Map<String, dynamic> data) async {
+  static Future<List<dynamic>> getLoginPow(Map<String, dynamic> data) async {
     return await req(
       url: '/auth/get-login-pow',
       data: data,
     );
   }
 
-  // 登录
-  static login(Map<String, dynamic> data) async {
+  static Future<List<dynamic>> login(Map<String, dynamic> data) async {
     return await req(
       url: '/auth/login',
       data: data,
     );
   }
 
-  // 换票
-  static refreshToken(Map<String, dynamic> data) async {
+  static Future<List<dynamic>> refreshToken(Map<String, dynamic> data) async {
     return await req(
       url: '/auth/refresh-token',
       data: data,
