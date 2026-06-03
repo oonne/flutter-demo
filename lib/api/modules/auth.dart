6e1211be1f23ee0adb7061e1750e06be0cda4970
JAY <JAY@oonne.com>
2026-06-03 16:39:37 +0800

    feat: 类型定义

--- lib/api/modules/auth.dart
+++ lib/api/modules/auth.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_demo/api/http.dart';
 
 class AuthApi {
+  // 获取登录pow
   static Future<List<dynamic>> getLoginPow(Map<String, dynamic> data) async {
     return await req(
       url: '/auth/get-login-pow',
@@ -8,6 +9,7 @@ class AuthApi {
     );
   }
 
+  // 登录
   static Future<List<dynamic>> login(Map<String, dynamic> data) async {
     return await req(
       url: '/auth/login',
@@ -15,11 +17,11 @@ class AuthApi {
     );
   }
 
+  // 换票
   static Future<List<dynamic>> refreshToken(Map<String, dynamic> data) async {
     return await req(
       url: '/auth/refresh-token',
       data: data,
     );
   }
-  
 }
--- lib/api/modules/redeem.dart
+++ lib/api/modules/redeem.dart
@@ -2,7 +2,7 @@ import 'package:flutter_demo/api/http.dart';
 
 class RedeemApi {
   // 查询兑换码列表
-  static  Future<List<dynamic>> getList(Map<String, dynamic> data) async {
+  static Future<List<dynamic>> getList(Map<String, dynamic> data) async {
     return await req(
       url: '/redeem/get-list',
       data: data,
